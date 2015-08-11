b8425bd4ff348423d9f8344b935aae38ce2fa90d
Stevearzh <zy911004@gmail.com>
2015-08-11 15:17:13 +0800

    update

--- build/js/qse.js
+++ build/js/qse.js
@@ -2,11 +2,28 @@ function $id(id) {
   return document.getElementById(id);
 }
 
+HTMLElement.prototype.hasClass = function(className) {
+  return !!this.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+}
+
+HTMLElement.prototype.addClass = function(className) {
+  if (!this.hasClass(className)) {
+    this.className += ' ' + className;
+  }
+}
+
+HTMLElement.prototype.removeClass = function(className) {
+  if (this.hasClass(className)) {
+    var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+    this.className = this.className.replace(reg, ' ');
+  }
+}
+
 function newNode(parentNodeId, param) {
-  var element;  
+  var element;
+  
   if (param['isTextNode']) {
     element = document.createTextNode(param['text']);
-    $id(parentNodeId).appendChild(element);
   } else {
     element = document.createElement(param['nodeType']);
     param['className'] ? element.className = param['className'] : '';
@@ -16,185 +33,173 @@ function newNode(parentNodeId, param) {
     param['name'] ? element.name = param['name'] : '';
     param['type'] ? element.type = param['type'] : '';
     param['value'] ? element.value = param['value'] : '';
-    $id(parentNodeId).appendChild(element);
   }
-}
-
-function hasClass(ele,cls) {
-  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
-}
-
-function addClass(ele,cls) {
-  if (!hasClass(ele,cls)) ele.className += " "+cls;
-}
-
-function removeClass(ele,cls) {
-  if (hasClass(ele,cls)) {
-    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
-    ele.className=ele.className.replace(reg,' ');
-  }
+  
+  $id(parentNodeId).appendChild(element);
 }
 
 // generate editor
+(function() {
+  $id('qse').className = 'qse';
 
-$id('qse').className = 'qse';
+  newNode('qse', {
+    'nodeType': 'div',
+    'className': 'qse-nav',
+    'id': 'qseNav'
+  });
 
-newNode('qse', {
-  'nodeType': 'div',
-  'className': 'qse-nav',
-  'id': 'qseNav'
-});
+  newNode('qseNav', {
+    'nodeType': 'div',
+    'className': 'qse-mode qse-md',
+    'id': 'qseMd'
+  });
 
-newNode('qseNav', {
-  'nodeType': 'div',
-  'className': 'qse-mode qse-md',
-  'id': 'qseMd'
-});
+  newNode('qseMd', {
+    'isTextNode': true,
+    'text': 'Markdown'
+  });
 
-newNode('qseMd', {
-  'isTextNode': true,
-  'text': 'Markdown'
-});
+  newNode('qseNav', {
+    'nodeType': 'div',
+    'className': 'qse-mode qse-bbc qse-mode-clear',
+    'id': 'qseBBC'
+  });
 
-newNode('qseNav', {
-  'nodeType': 'div',
-  'className': 'qse-mode qse-bbc qse-mode-clear',
-  'id': 'qseBBC'
-});
+  newNode('qseBBC', {
+    'isTextNode': true,
+    'text': 'BBCode'
+  });
 
-newNode('qseBBC', {
-  'isTextNode': true,
-  'text': 'BBCode'
-});
+  newNode('qseNav', {
+    'nodeType': 'div',
+    'className': 'qse-action qse-preview',
+    'id': 'qsePreview'
+  });
 
-newNode('qseNav', {
-  'nodeType': 'div',
-  'className': 'qse-action qse-preview',
-  'id': 'qsePreview'
-});
+  newNode('qsePreview', {
+    'isTextNode': true,
+    'text': '预览'
+  });
 
-newNode('qsePreview', {
-  'isTextNode': true,
-  'text': '预览'
-});
+  newNode('qseNav', {
+    'nodeType': 'div',
+    'className': 'qse-action qse-fullscreen',
+    'id': 'qseFullscreen'
+  });
 
-newNode('qseNav', {
-  'nodeType': 'div',
-  'className': 'qse-action qse-fullscreen',
-  'id': 'qseFullscreen'
-});
+  newNode('qseFullscreen', {
+    'isTextNode': true,
+    'text': '全屏'
+  });
 
-newNode('qseFullscreen', {
-  'isTextNode': true,
-  'text': '全屏'
-});
+  newNode('qse', {
+    'nodeType': 'div',
+    'className': 'qse-face',
+    'id': 'qseFace'
+  });
 
-newNode('qse', {
-  'nodeType': 'div',
-  'className': 'qse-face',
-  'id': 'qseFace'
-});
+  newNode('qseFace', {
+    'nodeType': 'div',
+    'className': 'qse-main',
+    'id': 'qseMain'
+  });
 
-newNode('qseFace', {
-  'nodeType': 'div',
-  'className': 'qse-main',
-  'id': 'qseMain'
-});
+  newNode('qseMain', {
+    'nodeType': 'textarea',
+    'className': 'qse-area',
+    'id': 'qseArea'
+  });
 
-newNode('qseMain', {
-  'nodeType': 'textarea',
-  'className': 'qse-area',
-  'id': 'qseArea'
-});
+  newNode('qseMain', {
+    'nodeType': 'form',
+    'className': 'qse-img-uploader',
+    'id': 'qseImgUploader',
+    'enctype': 'multipart/form-data',
+    'method': 'POST'
+  });
 
-newNode('qseMain', {
-  'nodeType': 'form',
-  'className': 'qse-img-uploader',
-  'id': 'qseImgUploader',
-  'enctype': 'multipart/form-data',
-  'method': 'POST'
-});
+  newNode('qseImgUploader', {
+    'nodeType': 'fieldset',
+    'id': 'innerUploader'
+  });
 
-newNode('qseImgUploader', {
-  'nodeType': 'fieldset',
-  'id': 'innerUploader'
-});
+  newNode('innerUploader', {
+    'nodeType': 'input',
+    'className': 'qse-img-uploader-hidden',
+    'id': 'MAX_FILE_SIZE',
+    'name': 'MAX_FILE_SIZE',
+    'type': 'hidden',
+    'value': 3000
+  });
 
-newNode('innerUploader', {
-  'nodeType': 'input',
-  'className': 'qse-img-uploader-hidden',
-  'id': 'MAX_FILE_SIZE',
-  'name': 'MAX_FILE_SIZE',
-  'type': 'hidden',
-  'value': 3000
-});
+  newNode('innerUploader', {
+    'nodeType': 'div',
+    'id': 'innerDiv'
+  });
 
-newNode('innerUploader', {
-  'nodeType': 'div',
-  'id': 'innerDiv'
-});
+  newNode('innerDiv', {
+    'nodeType': 'input',
+    'className': 'qse-img-uploader-drag',
+    'id': 'qseFileSelect',
+    'multiple': 'multiple',
+    'name': 'fileselect[]',
+    'type': 'file'
+  });
 
-newNode('innerDiv', {
-  'nodeType': 'input',
-  'className': 'qse-img-uploader-drag',
-  'id': 'qseFileSelect',
-  'multiple': 'multiple',
-  'name': 'fileselect[]',
-  'type': 'file'
-});
+  newNode('innerDiv', {
+    'nodeType': 'div',
+    'className': 'qse-img-uploader-information',
+    'id': 'qseImgUploaderInfo'
+  });
 
-newNode('innerDiv', {
-  'nodeType': 'div',
-  'className': 'qse-img-uploader-information',
-  'id': 'qseImgUploaderInfo'
-});
+  newNode('qseImgUploaderInfo', {
+    'isTextNode': true,
+    'text': '拖动图片至此处，复制粘贴，或点击'
+  });
 
-newNode('qseImgUploaderInfo', {
-  'isTextNode': true,
-  'text': '拖动图片至此处，复制粘贴，或点击'
-});
+  newNode('qseImgUploaderInfo', {
+    'nodeType': 'span',
+    'className': 'qse-img-uploader-click-here',
+    'id': 'qseClickHere'
+  });
 
-newNode('qseImgUploaderInfo', {
-  'nodeType': 'span',
-  'className': 'qse-img-uploader-click-here',
-  'id': 'qseClickHere'
-});
+  newNode('qseClickHere', {
+    'isTextNode': true,
+    'text': '这里'
+  });
 
-newNode('qseClickHere', {
-  'isTextNode': true,
-  'text': '这里'
-});
+  newNode('qseImgUploaderInfo', {
+    'isTextNode': true,
+    'text': '上传'
+  });
 
-newNode('qseImgUploaderInfo', {
-  'isTextNode': true,
-  'text': '上传'
-});
+  newNode('qseFace', {
+    'nodeType': 'button',
+    'className': 'qse-submit',
+    'id': 'qseSubmit'
+  });
 
-newNode('qseFace', {
-  'nodeType': 'button',
-  'className': 'qse-submit',
-  'id': 'qseSubmit'
-});
-
-newNode('qseSubmit', {
-  'isTextNode': true,
-  'text': '提交'
-});
+  newNode('qseSubmit', {
+    'isTextNode': true,
+    'text': '提交'
+  });
+})();
 
 // qseditor UI
+(function() {
+  var qseMd = $id('qseMd');
+  var qseBBC = $id('qseBBC');
 
-var qseMd = $id('qseMd');
-var qseBBC = $id('qseBBC');
+  qseMd.onclick = function() {
+    qseMd.removeClass('qse-mode-clear');
+    qseBBC.addClass('qse-mode-clear');
+  }
 
-qseMd.onclick = function() {
-  removeClass(qseMd, 'qse-mode-clear');
-  addClass(qseBBC, 'qse-mode-clear');
-}
-
-qseBBC.onclick = function() {
-  removeClass(qseBBC, 'qse-mode-clear');
-  addClass(qseMd, 'qse-mode-clear');
-}
+  qseBBC.onclick = function() {
+    qseBBC.removeClass('qse-mode-clear');
+    qseMd.addClass('qse-mode-clear');
+  }
+})();
 
 //qseImgUploader
 function Output(msg) {
